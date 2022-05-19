2a6465cdc4c9f145369e4ba13e283df7a526e58c
Spynos <38928588+spynos@users.noreply.github.com>
2022-05-19 16:50:36 +0900

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import React, { useState, useEffect } from "react";
+import entities from "./entities/index";
+import Physics from "./physics";
 export default function App() {
+  const [running, setRunning] = useState(false);
+  const [gameEngine, setGameEngine] = useState(null);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => setRunning(true), []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 40,
+          fontWeight: "bold",
+          margin: 50,
+        }}
+      >
+        {score}
+      </Text>
+      <GameEngine
+        ref={(ref) => setGameEngine(ref)}
+        systems={[Physics]}
+        entities={entities()}
+        running={running}
+        onEvent={(e) => {
+          switch (e.type) {
+            case "game_over":
+              setRunning(false);
+              gameEngine.stop();
+              setScore(0);
+              break;
+            case "new_point":
+              setScore((score) => score + 1);
+          }
+        }}
+        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+      >
+        <StatusBar style="auto" hidden={true} />
+      </GameEngine>
+
+      {!running ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "black",
+              paddingHorizontal: 30,
+              paddingVertical: 10,
+            }}
+            onPress={() => {
+              setScore(0);
+              setRunning(true);
+              gameEngine.swap(entities());
+            }}
+          >
+            <Text style={{ fontWeight: "bold", color: "white", fontSize: 30 }}>
+              START GAME
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
@@ -13,8 +72,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
